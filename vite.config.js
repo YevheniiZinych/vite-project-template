e446811e1e7036635d4bd93193938423693f9dd0
ZYevhenii <zinich911@gmail.com>
2024-10-13 10:56:03 +0300

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
+import path from "path";
 import nunjucks from "vite-plugin-nunjucks";
 
 export default defineConfig({
   base: "/vite-project-template",
   build: {
-    outDir: "dist",
+    outDir: path.resolve(__dirname, "dist"),
+    emptyOutDir: true,
   },
-  root: "./src",
+  root: path.resolve(__dirname, "src"),
   plugins: [nunjucks()],
   server: {
     port: 4040,
