ebe36c24a9a6d91b86f7036c158421cde336cec5
ZYevhenii <zinich911@gmail.com>
2024-10-13 10:46:13 +0300

    add sass and nunjuks

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 import { defineConfig } from "vite";
+import nunjucks from "vite-plugin-nunjucks";
 
 export default defineConfig({
   base: "/vite-project-template",
+  root: "./src",
+  plugins: [nunjucks()],
+  server: {
+    port: 4040,
+    open: true,
+  },
 });
